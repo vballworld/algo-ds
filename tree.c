a130d9d5ad6a3ca0c5947647561ea0487e7147be
Joe <vballworld@gmail.com>
2017-07-09 14:07:43 -0400

    add binary tree APIs

--- tree.c
+++ tree.c
@@ -1,4 +1,94 @@
 /*
  binary tree implementation
  */
+
+#include <stdlib.h>
 #include "tree.h"
+
+/*static prototypes
+ */
+static tree_node_t* tree_initNode(int val);
+
+tree_t* tree_init(int val)
+{
+    tree_t* t = (tree_t*)malloc(sizeof(tree_t));
+    t->root = tree_initNode(val);
+    
+    return t;
+}
+
+static tree_node_t* tree_initNode(int val)
+{
+    tree_node_t* n = (tree_node_t*)malloc(sizeof(tree_node_t));
+    n->left = NULL;
+    n->right = NULL;
+    n->val = val;
+    
+    return n;
+}
+
+void tree_add(tree_node_t* root, int val)
+{
+    if (val <= root->val)
+    {
+        if (root->left == NULL)
+        {
+            root->left = tree_initNode(val);
+        }
+        else
+        {
+            tree_add(root->left, val);
+        }
+    }
+    else
+    {
+        if (root->right == NULL)
+        {
+            root->right = tree_initNode(val);
+        }
+        else
+        {
+            tree_add(root->right, val);
+        }
+    }
+}
+
+
+void tree_preOrder(tree_node_t* root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    
+    printf("%d ", root->val);
+    
+    tree_preOrder(root->left);
+    tree_preOrder(root->right);
+}
+
+void tree_inOrder(tree_node_t* root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    
+    tree_inOrder(root->left);
+    printf("%d ", root->val);
+    tree_inOrder(root->right);
+}
+
+void tree_postOrder(tree_node_t* root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    
+    tree_postOrder(root->left);
+    tree_postOrder(root->right);
+    printf("%d ", root->val);
+}
+
+
--- tree.h
+++ tree.h
@@ -7,4 +7,23 @@
 
 #include <stdio.h>
 
+typedef struct tree_node_t
+{
+    int val;
+    struct tree_node_t* left;
+    struct tree_node_t* right;
+} tree_node_t;
+
+typedef struct tree_t
+{
+    tree_node_t* root;
+} tree_t;
+
+tree_t* tree_init(int val);
+void tree_add(tree_node_t* root, int val);
+
+void tree_preOrder(tree_node_t* root);
+void tree_inOrder(tree_node_t* root);
+void tree_postOrder(tree_node_t* root);
+
 #endif /* tree_h */
