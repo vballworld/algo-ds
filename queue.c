b67008313b4df979d9429887d9a8323b55f03fa1
Joe <vballworld@gmail.com>
2017-07-09 10:20:18 -0400

    add empty queue.c/.h

---
+++ queue.c
@@ -0,0 +1,9 @@
+//
+//  queue.c
+//  ctci
+//
+//  Created by Joe Pak on 7/9/17.
+//  Copyright © 2017 Joe Pak. All rights reserved.
+//
+
+#include "queue.h"
---
+++ queue.h
@@ -0,0 +1,14 @@
+//
+//  queue.h
+//  ctci
+//
+//  Created by Joe Pak on 7/9/17.
+//  Copyright © 2017 Joe Pak. All rights reserved.
+//
+
+#ifndef queue_h
+#define queue_h
+
+#include <stdio.h>
+
+#endif /* queue_h */
